e5e0cd3288963c38ab4f99dee62425565ff6529d
Dave Sizer <dasizer@gmail.com>
2014-08-10 22:05:14 -0400

    Wrote a (maybe working) recursive procedure for generating full trees, began reimplementation of LOCTree because I'm dumb and did the wrong one

--- CS260PA2/TREE_LOC/LOCTree.h
+++ CS260PA2/TREE_LOC/LOCTree.h
@@ -12,39 +12,48 @@
 #ifndef LOCTREE_H_
 #define LOCTREE_H_
 
-#include <vector>
+#include <deque>
 #include <string>
 #include "PointerList.h"
 
 using namespace std;
 
-typedef int node;
+typedef int cs_cursor; // A cursor into the cell space
+typedef int ns_cursor; // A cursor into the nodespace
+
+template<typename T>
+struct tnode {
+	T label; // The label of the node
+	cs_cursor header; // A cursor to the node's leftmost child in cellspace
+};
+
+struct tcell {
+	ns_cursor node; // A cursor to this cell's node in nodespace
+	cs_cursor next; // A cursor to this cell's right sibling in cellspace
+};
 
 template<typename T>
 class LOCTree {
 
 protected: // We need to access these for when this tree will be merged with another one
-	vector<PointerList<node> > header;
-	vector<T> label;
-	node root;
-	node avail; // The next available position for a node
+	deque<tnode<T> > nodespace;
+	deque<tcell> cellspace;
+	ns_cursor root;
 
-private:
-	void RESIZE(int n); // Resize our vectors
 
 public:
 	LOCTree();
 	void CREATE0(T v);  // Makes this tree have a single node with value v
 	void CREATE1(T v); // Create a new node with value v, and makes it the root of this tree
-	void CREATE2(T v, LOCTree & t); // Create a new node v, and set this tree and t as it's children (merge this with t and set v as root)
-	void CREATE3(T v, LOCTree & t1, LOCTree & t2); // Create new node v, and set this, t1 and t2 as it's children (three-way merge with v as root)
-	node ROOT();
-	node LEFTMOST_CHILD(node n);
-	node RIGHT_SIBLING(node n);
-	node PARENT(node n);
-	T LABEL(node n);
+	void CREATE2(T v, LOCTree t); // Create a new node v, and set this tree and t as it's children (merge this with t and set v as root)
+	void CREATE3(T v, LOCTree t1, LOCTree t2); // Create new node v, and set this, t1 and t2 as it's children (three-way merge with v as root)
+	ns_cursor ROOT();
+	ns_cursor LEFTMOST_CHILD(ns_cursor);
+	ns_cursor RIGHT_SIBLING(ns_cursor);
+	ns_cursor PARENT(ns_cursor);
+	T LABEL(ns_cursor);
 	void MAKENULL();
-	void PRINTTREE(node n, string indent, bool last);
+	void PRINTTREE(ns_cursor n, string indent, bool last);
 };
 
 template<typename T>
@@ -52,160 +61,94 @@ LOCTree<T>::LOCTree()
 {
 	// The root will be at 1, 0 will represent null
 	root = 1;
-	avail = 1;
-
-	// Start off with 10 spaces
-	header.resize(10);
-	label.resize(10);
-}
-
-template<typename T>
-void LOCTree<T>::RESIZE(int n)
-{
-	if (n > header.size() - 1) // header and label will always be the same size
-	{
-		header.resize(n+1);
-		label.resize(n+1);
-	}
+	nodespace.clear();
+	cellspace.clear();
+	nodespace.resize(1);
+	cellspace.resize(1);
 }
 
 template<typename T>
 void LOCTree<T>::CREATE0(T v)
 {
-	if (!header.empty())
-		header.clear();
-	if (!label.empty())
-		label.clear();
-	avail = 1;
-	RESIZE(avail);
-
-	header[avail] = *(new PointerList<node>);
-	label[avail] = v;
-	avail++;
+	LOCTree();
+	tnode<T> newn;
+	newn.label = v;
+	newn.header = 0;
+	nodespace.push_back(newn);
+	root = 1;
 
 }
 
 template<typename T>
 void LOCTree<T>::CREATE1(T v)
 {
-	RESIZE(avail);
+	// Create the node for the new root, and set the root here
+	tnode<T> newn;
+	newn.label = v;
+	root = nodespace.size(); // Since we haven't added it yet
 
-	// Move all other nodes down
-	for (int i = avail-1; i >= 1; i--)
-	{
-		header[i+1] = header[i];
-		label[i+1] = label[i];
-	}
+	// Create a cell entry for the old root
+	tcell newc;
+	newc.node = nodespace.size()-1; // Set the node to the old root
+	newc.next = 0;
+	cellspace.push_back(newc);
 
-	// Create a new node with value v at root position
-	header[ROOT()] = *(new PointerList<node>);
-	label[ROOT()] = v;
+	// Set the root's child to the old root
+	newn.header = cellspace.size()-1;
 
-	// Set its child to be the old root
-	header[ROOT()].INSERT(ROOT() + 1, header[ROOT()].END());
-	avail++;
+	// Finally, add the new root to nodespace
+	nodespace.push_back(newn);
+
+
 }
 
 template<typename T>
-void LOCTree<T>::CREATE2(T v, LOCTree<T> & t)
+void LOCTree<T>::CREATE2(T v, LOCTree<T> t)
 {
-	// We will CREATE1, then copy t's nodes into this tree, and add its root to this root's children
 
-	CREATE1(v);
-	RESIZE((avail+1) + (t.avail + 1)); // Make sure vectors are big enough to accommodate both trees
-
-	for (int i = 1; i < t.avail; i++)
-	{
-		header[avail+i] = t.header[i];
-		label[avail+i] = t.label[i];
-	}
-
-	header[ROOT()].INSERT(avail, header[ROOT()].END());
-
-	avail+=t.avail;
 }
 
 template<typename T>
-void LOCTree<T>::CREATE3(T v, LOCTree<T> & t1, LOCTree<T> & t2)
+void LOCTree<T>::CREATE3(T v, LOCTree<T> t1, LOCTree<T> t2)
 {
-	// CREATE 2, and then follow a similar procedure to merge the 3rd tree
 
-	CREATE2(v, t1);
-	RESIZE((avail+1) + (t2.avail + 1)); // Make sure vectors are big enough to accommodate both trees
-
-	for (int i = 1; i < t2.avail; i++)
-	{
-		header[avail+i] = t2.header[i];
-		label[avail+i] = t2.label[i];
-	}
-
-	header[ROOT()].INSERT(avail, header[ROOT()].END());
-
-	avail+=t2.avail;
 }
 
 template<typename T>
-node LOCTree<T>::ROOT()
+ns_cursor LOCTree<T>::ROOT()
 {
 	return root;
 }
 
 template<typename T>
-T LOCTree<T>::LABEL(node n)
+T LOCTree<T>::LABEL(ns_cursor n)
 {
-	return label[n];
+	return nodespace[n].label;
 }
 
 template<typename T>
-node LOCTree<T>::LEFTMOST_CHILD(node n)
+ns_cursor LOCTree<T>::LEFTMOST_CHILD(ns_cursor n)
 {
-	return header[n].RETRIEVE(header[n].FIRST());
+	return cellspace[nodespace[n].header].node;
 }
 
 template<typename T>
-node LOCTree<T>::RIGHT_SIBLING(node n)
+ns_cursor LOCTree<T>::RIGHT_SIBLING(ns_cursor n)
 {
-	// Locate the child of n's parent that occurs after n
-	node parent = PARENT(n);
-
-	cell<node>* p = header[parent].FIRST();
-
-	while (p->next != NULL)
-	{
-		p = p->next;
-		if (p->element == n)
-			break;
-	}
-
-	if (p->next == NULL)
-		return 0;
-	else return p->next->element;
+	return cellspace[nodespace[n].header].next;
 }
 
 template<typename T>
-node LOCTree<T>::PARENT(node n)
+ns_cursor LOCTree<T>::PARENT(ns_cursor n)
 {
-	// Locate the node that has n as its child
-	for (int i = 1; i < avail; i++)
-	{
-		cell<node>* p = header[i].FIRST();
-		while(p->next != NULL)
-		{
-			p = p->next;
-			if (p->element == n)
-				return i;
-		}
-	}
 
-	// Return "null" if not found
-	return 0;
 }
 
 template<typename T>
 void LOCTree<T>::MAKENULL()
 {
-	header.clear();
-	label.clear();
+	nodespace.clear();
+	cellspace.clear();
 	LOCTree();
 }
 
@@ -226,8 +169,8 @@ void LOCTree<T>::PRINTTREE(node n, string indent, bool last)
 	}
 	cout << label[n] << endl;
 
-	cell<node>* i = header[n].FIRST();
-	cell<node>* end = header[n].END();
+	cell<node>* i = header[n]->FIRST();
+	cell<node>* end = header[n]->END();
 	while (i->next != NULL)
 	{
 		i = i->next;
--- CS260PA2/TREE_LOC/LOCTreeTest.cpp
+++ CS260PA2/TREE_LOC/LOCTreeTest.cpp
@@ -4,11 +4,55 @@ using namespace std;
 
 #include "LOCTree.h"
 
+template<typename T>
+LOCTree<T> createFullTree(T&, int);
+
 int main()
 {
-	LOCTree<int> ltree;
-	ltree.CREATE0(5);
-	ltree.PRINTTREE(ltree.ROOT(), "", false);
+	/*
+	LOCTree<int> tree;
+	tree.CREATE0(0);
+	LOCTree<int>* subtrees[2];
+	for (int i = 0; i < 2; i++)
+	{
+		subtrees[i] = new LOCTree<int>;
+		subtrees[i]->CREATE0(i+1);
+	}
+	tree.CREATE3(10, *(subtrees[0]), *(subtrees[1]));
+	tree.PRINTTREE(tree.ROOT(), "", false);
 
+	cout << tree.LABEL(tree.ROOT()) << endl;
+	cout << tree.LABEL(tree.LEFTMOST_CHILD(tree.ROOT()));
+*/
+
+	int rootval = 0;
+	LOCTree<int> generatedTree = createFullTree(rootval,2);
+	//generatedTree.PRINTTREE(generatedTree.ROOT(), "", false);
 	return 0;
+}
+
+template<typename T>
+LOCTree<T> createFullTree( T & rootval, int height)
+{
+	cout << "Height: " << height << endl;
+	LOCTree<T> t1, t2, t3;
+	if (height == 1)
+	{
+		t1.CREATE0(++rootval);
+		t2.CREATE0(++rootval);
+		t3.CREATE0(++rootval);
+		t1.CREATE3(rootval-3, t2, t3);
+		cout << "Height 1 Tree FINAL:" << endl;
+		t1.PRINTTREE(t1.ROOT(), "", false);
+	}
+	else if (height > 1)
+	{
+		t1 = createFullTree(++rootval, height-1);
+		t2 = createFullTree(++rootval, height-1);
+		t3 = createFullTree(++rootval, height-1);
+
+		t1.CREATE3(0, t2, t3);
+		//t1.PRINTTREE(t1.ROOT(), "", false);
+	}
+	return t1;
 }
